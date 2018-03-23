e869a453f10dc0843dab8e83794eb4d3ec77ae31
Johannes <johanneskamph@gmail.com>
2018-03-23 11:18:43 +0100

    player

--- www/js/registration.js
+++ www/js/registration.js
@@ -1,14 +1,25 @@
 $(document).ready(function() {
+	$("#playbutton").click(function(){
+		let p1Name = ("p1").val() 
+		let p2Name = ("p2").val()
 
-	let p1 = ("p1").val() 
-	let p2 = ("p2").val()
+let p1Bot = ("#bot1").is(':checked')
+let p2Bot = ("#bot2").is(':checked')
 
+		let p1 = {
+			name: p1name,
+			isBot: p1bot
+		}
 
-
-
+		let p2 = {
+			name: p2name,
+			isBot: p2bot
+		}
 
 
 	   JSON._save('registration', [p1,p2]);
+
+	})
 })
 
 
