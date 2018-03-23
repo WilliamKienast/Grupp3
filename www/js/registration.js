23c0e960c802e0055433467728b93650eabe4e74
Johannes <johanneskamph@gmail.com>
2018-03-23 13:57:10 +0100

    registration fix

--- www/js/registration.js
+++ www/js/registration.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
+	
 	$("#playbutton").click(function(){
-		let p1Name = ("p1").val() 
-		let p2Name = ("p2").val()
+		let p1Name = $("#p1").val() 
+		let p2Name = $("#p2").val()
 
-let p1Bot = ("#human1").is(':checked')
-let p2Bot = ("#human2").is(':checked')
+		let p1Bot = $("#bot1").is(':checked')
+		let p2Bot = $("#bot2").is(':checked')
 
 		let p1 = {
-			name: p1name,
-			isBot: p1bot
+			name: p1Name,
+			isBot: p1Bot
 		}
 
 		let p2 = {
-			name: p2name,
-			isBot: p2bot
+			name: p2Name,
+			isBot: p2Bot
 		}
 
-
-	   JSON._save('registration', [p1,p2]);
+	   JSON._save('registration', [p1, p2]);
 
 	})
 })
