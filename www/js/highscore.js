d6d075637e4b53fd05772b0f7794a1660fd5d8b4
Tryhardmatte <tryhardmatte@gmail.com>
2018-03-28 11:01:01 +0200

    fix bugg

--- www/js/highscore.js
+++ www/js/highscore.js
@@ -63,19 +63,6 @@ class HighScore {
              '</div>';
 
         }
-        var table = document.getElementById("myTable");
-
-// Create an empty <tr> element and add it to the 1st position of the table:
-var row = table.insertRow(0);
-
-// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-var cell1 = row.insertCell(0);
-var cell2 = row.insertCell(1);
-
-// Add some text to the new cells:
-cell1.innerHTML = "NEW CELL1";
-cell2.innerHTML = "NEW CELL2";
-
         // Let jQuery grab the main element/tag and replace everything inside it 
         // with the content of x
         $('main').html(x);
